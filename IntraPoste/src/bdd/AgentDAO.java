aae24f76b2f8517e1bb186a36076e8c3ed422187
Bonnin Emilie <ancoly2804@msn.com>
2012-11-05 15:44:01 +0100

    DAO Agent / ErreurCaisse

--- IntraPoste/src/bdd/AgentDAO.java
+++ IntraPoste/src/bdd/AgentDAO.java
@@ -1,19 +1,67 @@
 package bdd;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 
 import metier.Agent;
+import metier.ErreurCaisse;
 
 public class AgentDAO {
 
 	public static Agent selectByCode(String codeAgent) {
-		// TODO Auto-generated method stub
-		return null;
+			try {
+				Statement select = Connexion.getInstance().getConnection().createStatement();
+				ResultSet result = select
+						.executeQuery("SELECT * FROM AGENT WHERE CODE_AGENT = '"
+								+ codeAgent + "'");
+				if (result.next())
+				{
+//					System.out.println(result.getString("CODE_AGENCE"));
+					return (new Agent(result.getString("CODE_AGENT"), 
+							AgenceDAO.selectByCode(result.getString("CODE_AGENCE")), 
+							TypeAgentDAO.selectByCode(result.getInt("CODE_TYPE_AGENT")), 
+							result.getString("MAIL"), 
+							result.getString("NOM"), 
+							result.getString("PRENOM"), 
+							result.getString("MOT_DE_PASSE")));
+				}
+				
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return null;
 	}
 
-	public static ArrayList<Agent> selectByAgence(String codeAgence) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+	public static ArrayList<Agent> selectByAgence(String codeAgence) throws SQLException {
+			ArrayList<Agent> results = new ArrayList<Agent>();
+			Statement select = null;
+			try {
+				select = Connexion.getInstance().getConnection().createStatement();
+				String query = "SELECT * FROM AGENT WHERE CODE_AGENCE = '"
+						+ codeAgence + "'";
+				ResultSet result = select.executeQuery(query);
+
+				while (result.next()) {
+					results.add(new Agent(result.getString("CODE_AGENT"), 
+							AgenceDAO.selectByCode(result.getString("CODE_AGENCE")), 
+							TypeAgentDAO.selectByCode(result.getInt("CODE_TYPE_AGENT")), 
+							result.getString("MAIL"), 
+							result.getString("NOM"), 
+							result.getString("PRENOM"), 
+							result.getString("MOT_DE_PASSE")));
+				}
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				if (select != null)
+					select.close();
+			}
+			return results;
+		}
 
 }
--- IntraPoste/src/bdd/ErreurCaisseDAO.java
+++ IntraPoste/src/bdd/ErreurCaisseDAO.java
@@ -7,10 +7,36 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+import metier.Agence;
 import metier.ErreurCaisse;
 
 public class ErreurCaisseDAO {
 
+	public static ErreurCaisse selectByCode(String codeErreurCaisse) {
+
+		try {
+			Statement select = Connexion.getInstance().getConnection().createStatement();
+			ResultSet result = select
+					.executeQuery("SELECT * FROM ERREUR_CAISSE WHERE ERREUR_CAISSE_ID = '"
+							+ codeErreurCaisse + "'");
+			if (result.next())
+			{
+//				System.out.println(result.getString("ERREUR_CAISSE_ID"));
+				return (new ErreurCaisse(result.getInt("ERREUR_CAISSE_ID"),
+						result.getString("CODE_AGENT"), 
+						result.getString("CODE_TYPE_ERREUR"), 
+						result.getString("CODE_AGENCE"), 
+						result.getInt("CODE_STATUT_REGULARISATION"), 
+						result.getDate("DATE_VACATION"), 
+						result.getFloat("MONTANT")));
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
 	public static ArrayList<ErreurCaisse> selectErreursCaisseForBilanJournee(
 			String codeAgence, java.util.Date dateVacation,
 			String codeTypeErreur, int codeStatusRegularisation)
@@ -131,6 +157,42 @@ public class ErreurCaisseDAO {
 		}
 		return results;
 	}
+
+	public static ArrayList<ErreurCaisse> selectErreursCaisseByAgent(
+			String codeAgent, Date dateVacation, String codeTypeErreur) throws SQLException {
+		Statement select = null;
+		ArrayList<ErreurCaisse> results = new ArrayList<ErreurCaisse>();
+		try {
+			select = Connexion.getInstance().getConnection().createStatement();
+			String query = "SELECT * FROM ERREUR_CAISSE WHERE CODE_AGENT = '"
+					+ codeAgent
+					+ "' AND DATE_VACATION = '"
+					+ dateVacation
+					+ "' AND CODE_TYPE_ERREUR = '"
+					+ codeTypeErreur
+					+ "'";
+
+			ResultSet result = select.executeQuery(query);
+
+			while (result.next()) {
+				results.add(new ErreurCaisse(result.getInt("ERREUR_CAISSE_ID"),
+						result.getString("CODE_AGENT"), result
+								.getString("CODE_TYPE_ERREUR"), result
+								.getString("CODE_AGENCE"), result
+								.getInt("STATUS_REGULARISATION"), result
+								.getDate("DATE_VACATION"), result
+								.getFloat("MONTANT")));
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (select != null)
+				select.close();
+		}
+		return results;
+	}
+	
 	
 	public static ArrayList<ErreurCaisse> selectErreursCaisseByAgence(
 			String codeAgence, Date dateDebut, Date dateFin, String codeTypeErreur,
@@ -172,10 +234,23 @@ public class ErreurCaisseDAO {
 	}
 	
 	public static void insert(String codeAgence, String codeAgent,
-			Date dateVacation, String typeErreur, float montant) {
-		// TODO Auto-generated method stub
-		// Ne pas oublier de mettre le "CODE_STATUS_REGULARISATION" à 0 (erreur
-		// déclarée et non encore régularisée)
+			Date dateVacation, String typeErreur, float montant) throws SQLException {
+		Statement insert = null;
+		try {
+			if (selectErreursCaisseByAgent(codeAgent, dateVacation, typeErreur) == null) {
+				insert = Connexion.getInstance().getConnection()
+						.createStatement();
+				insert.executeQuery("INSERT INTO ERREUR_CAISSE VALUES ('', '"
+						+ codeAgent + "', '" + typeErreur + "', '" + codeAgence + 
+						"', 0 , '" + dateVacation + "', " + montant + ")");
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			if (insert != null)
+				insert.close();
+		}
 	}
 
 	public static boolean updateStatus(int erreurCaisseId, int nouveauCodeStatusRegularisation) {
